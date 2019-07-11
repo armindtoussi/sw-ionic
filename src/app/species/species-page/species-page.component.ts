ff6dc6cd678e68477c0b0b3f32b182e772f3e210
Armin Toussi <armintoussi@outlook.com>
2019-07-11 14:59:10 -0700

    added click function to species pages, comment, code clean up

--- src/app/species/species-page/species-page.component.ts
+++ src/app/species/species-page/species-page.component.ts
@@ -18,31 +18,66 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./species-page.component.scss'],
 })
 export class SpeciesPageComponent implements OnInit, OnDestroy {
-
+  /** Species subs. */
   speciesSubs: Subscription[];
-  
+  /** Data holding vars. */
   data:   Species;
   films:  Film[];
   people: Character[];
 
+  /**
+   * ctor
+   * @param route  Activated route ref.
+   * @param router router ref. 
+   * @param _toast toast presentation service. 
+   * @param _cache the caching service. 
+   */
   constructor(private route:  ActivatedRoute,
               private router: Router,
               private _cache: CacheService,
               private _toast: ToastService) { }
-
+  
+  /**
+   * lifecycle hook runs when component is being created. 
+   * Handles data. 
+   */
   ngOnInit(): void {
     this.speciesSubs = [];
     this.handleData();
   }
 
+  /**
+   * lifecycle hook runs when component is destroyed. 
+   * Unsubs to subs.
+   */
   ngOnDestroy(): void { 
     this.unsubscribe();
   }
-
+  
+  /**
+   * Determines if it's a number.
+   * @param arg value to check if it's a number.
+   */
   isNumber(arg: any): boolean  {
     return !isNaN(parseFloat(arg)) && !isNaN(arg - 0);
   }
 
+    /**
+   * Navigates to a sub-page, for characters, planets etc. 
+   * @param id the id/name of the element.
+   * @param segment the type of element.
+   */
+  navToElementPage(id: string, segment: string): void {
+    if(id && segment) {
+      this.router.navigateByUrl(`/${segment}/${id}`);
+    } else {
+      this.presentToast(`/`);
+    }
+  }
+
+  /**
+   * Gets main species data in the case of a reload or manual nav to this page. 
+   */
   private getSpecies(): void {
     let id = this.parsePath();
 
@@ -52,14 +87,14 @@ export class SpeciesPageComponent implements OnInit, OnDestroy {
           this.data = data.results[0];
           this.getExtraData();
         } else {
-          this._toast.presentToast(environment.notFound).then(
-            (res: any) => {
-              this.router.navigateByUrl(`/`);
-            });
+          this.presentToast(`/`);
         }
       });
   }
 
+  /**
+   * Fetches films that feature this species.
+   */
   private fetchFilms(): void {
     if(this.data.films.length === 0) {
       return;
@@ -67,12 +102,14 @@ export class SpeciesPageComponent implements OnInit, OnDestroy {
 
     this.speciesSubs[1] = this._cache.fetch(this.data.films)
       .subscribe((data: any) => {
-        this.films = data.sort((a: Film, b: Film) => {
-          return a.episode_id - b.episode_id;
-        });
+        this.films = data.sort((a: Film, b: Film) => 
+                     this.sortArr(a.episode_id.toString(), b.episode_id.toString()));
       });
   }
 
+  /**
+   * Fetches Characters associated to this movie. 
+   */
   private fetchPeople(): void {
     if(this.data.people.length === 0) {
       return; 
@@ -80,16 +117,22 @@ export class SpeciesPageComponent implements OnInit, OnDestroy {
 
     this.speciesSubs[2] = this._cache.fetch(this.data.people)
       .subscribe((data: any) => {
-        this.people = data;
+        this.people = data.sort((a: Character, b: Character) => this.sortArr(a.name, b.name));
       });
   }
 
+  /**
+   * Parses path to get id segment from url path. 
+   */
   private parsePath(): any {
     let idx = this.router.url.lastIndexOf('/');
     let id  = this.router.url.slice(idx + 1);
     return id;
   }
 
+  /**
+   * Handles main data on load of page. 
+   */
   private handleData(): void { 
     if(this.route.snapshot.data['special']) {
       this.data = this.route.snapshot.data['special'];
@@ -99,6 +142,29 @@ export class SpeciesPageComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * String sort function. 
+   * @param a string to sort 
+   * @param b string to sort
+   */
+  private sortArr(a: string, b: string): number {
+    return (a).localeCompare(b);
+  }
+
+  /**
+   * Helper function to present not found toast, and redirect.
+   * @param url url to redirect to.
+   */
+  private async presentToast(url: string): Promise<void> {
+    await this._toast.presentToast(environment.notFound).then( 
+      () => {
+        this.router.navigateByUrl(url);
+    });
+  }
+
+  /**
+   * Fetches extra data related to a page. 
+   */
   private getExtraData(): void {
     this.fetchFilms();
     this.fetchPeople();
--- src/app/species/species.page.ts
+++ src/app/species/species.page.ts
@@ -18,29 +18,48 @@ import { Species } from '../models/species.model';
   styleUrls: ['./species.page.scss'],
 })
 export class SpeciesPage implements OnInit, OnDestroy {
-
+  /** View child ref for infinit scroll element. */
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  speciesSub: Subscription[] = [];
+  /** Species subs. */
+  speciesSub: Subscription[];
+  /** Species fetch results. */
   species: Species[];
+  /** Url next set of results. */
   nextUrl: string;
+  /** Total count of results */
   count: number;
 
+  /**
+   * ctor.
+   * @param _swapiFetchService api fetch service layer.
+   * @param _dataService passing data service.
+   * @param router router.
+   */
   constructor(private _swapiFetchService: SwapiService,
               private _dataService: DataService,
               private router: Router) { }
 
-              
+  /**
+   * Lifecycle hook inits subs.
+   */    
   ngOnInit(): void {
+    this.speciesSub = [];
     this.getSpecies();
   }
 
+  /**
+   * lifecycle hook, unsubs. 
+   */
   ngOnDestroy(): void {
     this.unsubscribe();
   }
 
+  /**
+   * navigate to species display page
+   * @param species the species to display.
+   */
   displaySpecies(species: Species): void {
-    console.log("clicked: ", species);
     this._dataService.setData(species.name, species);
     this.router.navigateByUrl(`/species/${species.name}`);
   }
@@ -50,11 +69,12 @@ export class SpeciesPage implements OnInit, OnDestroy {
    * 
    * @param event the scroll event.
    */
-  loadData(event): void {
+  loadData(event: any): void {
     this.speciesSub[1] = this._swapiFetchService.genericFetch(this.nextUrl)
       .subscribe(
         (data: object) => {
-          this.species = this.species.concat(data['results']);
+          this.species = this.species.concat(data['results'])
+                                     .sort((a: Species, b: Species) => this.sortArr(a.name, b.name));
           this.nextUrl = data['next'];
           event.target.complete();
 
@@ -64,6 +84,9 @@ export class SpeciesPage implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Fetches the first 20 species results.
+   */
   private getSpecies(): void {
     this.speciesSub[0] = this._swapiFetchService.getSpecies()
       .pipe(
@@ -77,13 +100,24 @@ export class SpeciesPage implements OnInit, OnDestroy {
         (results: object) => {
           this.count = results['count'];
           this.nextUrl = results['next'];
-          this.species = this.species.concat(results['results']);
-
-          console.log("Species: ", this.species);
+          this.species = this.species.concat(results['results'])
+                                     .sort((a: Species, b: Species) => this.sortArr(a.name, b.name));
         }
       );
   }
+  
+  /**
+   * String sort function. 
+   * @param a string to sort 
+   * @param b string to sort
+   */
+  private sortArr(a: string, b: string): number {
+    return (a).localeCompare(b);
+  }
 
+  /**
+   * Unsubscribes from subs.
+   */
   private unsubscribe(): void {
     for(let i = 0; i < this.speciesSub.length; i++) {
       if(this.speciesSub[i] !== undefined) {
