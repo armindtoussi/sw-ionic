0f53c51787b73fa465f1bc8d51d93f1178a810d5
Armin Toussi <armintoussi@outlook.com>
2019-07-15 16:12:21 -0700

    added sw-icons, did color theme

--- src/app/services/nav-list.service.ts
+++ src/app/services/nav-list.service.ts
@@ -8,37 +8,37 @@ export class NavListService {
         {
             title: 'Home',
             url: '/home',
-            icon: 'home'
+            icon: 'assets/sw-icons/md-Star_Wars_Storm-Trooper.svg',
         },
         {
             title: 'Movies',
             url: '/movies',
-            icon: 'videocam',
+            icon: 'assets/sw-icons/md-Star_Wars_R2D2.svg',
         }, 
         {
             title: 'Characters',
             url: '/characters',
-            icon: 'people'
+            icon: 'assets/sw-icons/md-Star_Wars_Darth_Vader.svg'
         }, 
         {
             title: 'Planets',
             url: '/planets',
-            icon: 'planet'
+            icon: 'assets/sw-icons/md-Star_Wars_Death_Star.svg'
         }, 
         {
             title: 'Starships',
             url: '/starships',
-            icon: 'rocket'
+            icon: '/assets/sw-icons/md-Star_Wars_Falcon.svg'
         }, 
         {
             title: 'Species',
             url: '/species',
-            icon: 'paw'
+            icon: '/assets/sw-icons/md-Star_Wars_C3PO.svg'
         }, 
         {
             title: 'Vehicles',
             url: '/vehicles',
-            icon: 'car'
+            icon: '/assets/sw-icons/md-Star_Wars_BB8.svg'
         } 
     ];
 
